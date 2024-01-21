68a7dbb535cd6733485617348e1fe2668b575cbf
Nuria-Fernandez <154224975+Nuria-Fernandez@users.noreply.github.com>
2024-01-21 18:16:50 +0100

    Add files via upload

---
+++ N2exercici1/Main.java
@@ -0,0 +1,9 @@
+package N2exercici1;
+
+import N1exercici2.GenericMethods;
+
+public class Main {
+    public static void main(String[] args){
+        GenericMethods.metodoGenerico('K', "Hola", "Adiós");
+    }
+}
