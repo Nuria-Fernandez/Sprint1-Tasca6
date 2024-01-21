3487f9fa197114f4807352ce81e650caf0d36a24
Nuria-Fernandez <154224975+Nuria-Fernandez@users.noreply.github.com>
2024-01-21 18:17:42 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package N2exercici1;
-
-import N1exercici2.GenericMethods;
-
-public class Main {
-    public static void main(String[] args){
-        GenericMethods.metodoGenerico('K', "Hola", "Adiós");
-    }
-}
